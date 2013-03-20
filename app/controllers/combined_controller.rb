ddf693ad52614ee85543c7d1a5e311a25387e45b
Yahor Buben <yahor.buben@gmail.com>
2013-03-20 15:50:50 -0400

    updated

--- app/controllers/combined_controller.rb
+++
@@ -1,6 +0,0 @@
-class CombinedController < ApplicationController
-  def index
-    @users = User.all
-    @packages = Package.all
-  end
-end
--- app/controllers/frontshipper_controller.rb
+++
@@ -1,6 +0,0 @@
-class FrontShipperController < ApplicationController
-def index
-	@shipper = Shipper.all
-	@package = Package.all
-end
-end
